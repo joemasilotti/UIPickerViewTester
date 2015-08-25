3479c85aef95f9540c1168de758e9c48d8c6aa6a
Joe Masilotti <joseph.masilotti@gmail.com>
2015-08-25 09:05:46 -0400

    Add picker with the ten iPhone models

--- App/ViewController.swift
+++ App/ViewController.swift
@@ -9,5 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let iPhones = [ "iPhone", "iPhone 3G", "iPhone 3GS", "iPhone 4", "iPhone 4S", "iPhone 5", "iPhone 5C", "iPhone 5S", "iPhone 6", "iPhone 6 Plus" ]
+}
 
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return iPhones.count
+    }
+}
+
+extension ViewController: UIPickerViewDelegate{
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return iPhones[row]
+    }
 }
